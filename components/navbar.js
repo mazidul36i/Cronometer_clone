9d322f1aabfe9ef20bd4a07f04cb2156695f029a
jainendra singh <jainendrasingh398@gmail.com>
2022-08-25 16:26:50 +0530

    added file

---
+++ components/navbar.js
@@ -0,0 +1,23 @@
+
+       function navbar(){
+          return `<a href="#">
+        <img src="https://cdn1.cronometer.com/2021/landing/cronometer-logo.svg" alt="our main logo" class="logo">
+    </a>
+    <nav class="navbar">
+        <ul class="navbar-list">
+            <li><a href="#" class="navbar-link">Products</a> </li>
+            <li><a href="#" class="navbar-link">Support</a></li>
+            <li><a href="#" class="navbar-link">Blog</a></li>
+            <li><a href="#" class="navbar-link">Forums</a></li>
+            <li><a href="#" class="navbar-link">About</a></li>
+            <li><a href="#" class="navbar-link-a  a-button">Log In</a></li>
+        </ul>
+    </nav>
+    <div class="mobile-navbar-btn">
+        <ion-icon name="menu-outline" class="mobile-navbar-icon"></ion-icon>
+        <ion-icon name="close-circle-outline" class="mobile-navbar-icon"></ion-icon>
+    </div>`
+       }
+       
+       export default navbar();
+     
