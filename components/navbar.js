e417d28ba56b2c50050ad1ad2e4e6cc2b132472d
Mazidul36i <islammazidul1369@gmail.com>
2022-08-27 22:39:52 +0530

    Updated nav

--- components/navbar.js
+++ components/navbar.js
@@ -21,25 +21,36 @@ class navbar {
         </nav>
         <i class="mobile-navbar-btn fa-solid fa-bars"></i>
     </header>`;
+        this.func = `const nav_container = document.querySelector("#navbar");
+        let t;
+        // const toggleNavbar = () => {
+        //     if (t) {
+        //         clearTimeout(t);
+        //         t = null;
+        //     }
+        
+        //     // if (!nav_container.classList.contains("collapse")) {
+        //     //     t = setTimeout(() => {
+        //     //         nav_container.classList.toggle("collapse");
+        //     //     }, 600);
+        //     // } else {
+        //     //     nav_container.classList.toggle("collapse");
+        //     // }
+        // };
+        document
+            .querySelector(".mobile-navbar-btn")
+            .addEventListener("click", () => toggleNavbar());`;
+    }
 
-     function navbar(){
-          return `<a href="#">
-        <img src="https://cdn1.cronometer.com/2021/landing/cronometer-logo.svg" alt="our main logo" class="logo">
-    </a>
-    <nav class="navbar">
-        <ul class="navbar-list">
-            <li><a href="#" class="navbar-link">Products</a> </li>
-            <li><a href="#" class="navbar-link">Support</a></li>
-            <li><a href="#" class="navbar-link">Blog</a></li>
-            <li><a href="#" class="navbar-link">Forums</a></li>
-            <li><a href="#" class="navbar-link">About</a></li>
-            <li><a href="#" class="navbar-link-a  a-button">Log In</a></li>
-        </ul>
-    </nav>
-    <div class="mobile-navbar-btn">
-        <ion-icon name="menu-outline" class="mobile-navbar-icon"></ion-icon>
-        <ion-icon name="close-circle-outline" class="mobile-navbar-icon"></ion-icon>
-    </div>`
-       }
-       
-       export default navbar;
+    getNavbar() {
+        return this.nav;
+    }
+
+    getFunction(location) {
+        let scriptTag = document.createElement("script");
+        scriptTag.innerHTML = this.func;
+        console.log(scriptTag)
+        // location.appendChild(scriptTag);
+    }
+}
+export default new navbar();
