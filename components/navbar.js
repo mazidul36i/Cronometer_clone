4970007c671aa19367d722f7d67ef3d62749d11a
Mazidul36i <islammazidul1369@gmail.com>
2022-08-27 12:26:35 +0530

    Changed navbar and added as importable constructor function

--- components/navbar.js
+++ components/navbar.js
@@ -1,23 +1,57 @@
+class navbar {
+    constructor() {
+        this.nav = `<header id="navbar" class="collapse">
+        <a href="/">
+            <img src="https://cdn1.cronometer.com/2021/landing/cronometer-logo.svg" alt="our main logo" class="logo">
+        </a>
+        <nav>
+            <ul class="navbar-list">
+                <li>
+                    <div class="nav-product">
+                        <a>Products</a>
+                        <i class="fa-solid fa-angle-down"></i>
+                    </div>
+                </li>
+                <li><a href="./support.html" class="navbar-link">Support</a></li>
+                <li><a href="./blog.html" class="navbar-link">Blog</a></li>
+                <li><a href="./forums.html" class="navbar-link">Forums</a></li>
+                <li><a href="./about.html" class="navbar-link">About</a></li>
+                <li><a href="#" class="navbar-link-b">Log In</a></li>
+            </ul>
+        </nav>
+        <i class="mobile-navbar-btn fa-solid fa-bars"></i>
+    </header>`;
 
-     function navbar(){
-          return `<a href="#">
-        <img src="https://cdn1.cronometer.com/2021/landing/cronometer-logo.svg" alt="our main logo" class="logo">
-    </a>
-    <nav class="navbar">
-        <ul class="navbar-list">
-            <li><a href="#" class="navbar-link">Products</a> </li>
-            <li><a href="#" class="navbar-link">Support</a></li>
-            <li><a href="#" class="navbar-link">Blog</a></li>
-            <li><a href="#" class="navbar-link">Forums</a></li>
-            <li><a href="#" class="navbar-link">About</a></li>
-            <li><a href="#" class="navbar-link-a  a-button">Log In</a></li>
-        </ul>
-    </nav>
-    <div class="mobile-navbar-btn">
-        <ion-icon name="menu-outline" class="mobile-navbar-icon"></ion-icon>
-        <ion-icon name="close-circle-outline" class="mobile-navbar-icon"></ion-icon>
-    </div>`
-       }
-       
-       export default navbar();
-     
+        this.func = `const mobile_nav = document.querySelector(".mobile-navbar-btn");
+    const nav_header = document.querySelector("#navbar");
+    
+    let t;
+    const toggleNavbar = () => {
+        if (t) {
+            clearTimeout(t);
+            t = null;
+        }
+        nav_header.classList.toggle("active");
+        if (!nav_header.classList.contains("collapse")) {
+            t = setTimeout(() => {
+                nav_header.classList.toggle("collapse");
+            }, 600);
+        } else {
+            nav_header.classList.toggle("collapse");
+        }
+    };
+    mobile_nav.addEventListener("click", () => toggleNavbar());`;
+    }
+
+    getNavbar() {
+        return this.nav;
+    }
+
+    getFunction(location) {
+        var script = document.createElement("script");
+        script.innerHTML = this.func;
+        location.appendChild(script);
+    }
+}
+
+export default new navbar;
